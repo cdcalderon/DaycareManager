9bc7a1603843b656ac73b73ee6590cfb1cb4ae5d
carlos calderon <carlosdevelopment1979@gmail.com>
2016-05-13 19:52:40 -0500

    Initial Commit

---
+++ DaycareManager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DaycareManager
+//
+//  Created by carlos calderon on 5/13/16.
+//  Copyright © 2016 carlos calderon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
