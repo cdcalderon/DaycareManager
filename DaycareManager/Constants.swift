855b1ca36f9dff31f9f885fc5086f30a795822a2
carlos calderon <carlosdevelopment1979@gmail.com>
2016-05-14 21:00:49 -0500

    Add Login View

---
+++ DaycareManager/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  DaycareManager
+//
+//  Created by carlos calderon on 5/14/16.
+//  Copyright © 2016 carlos calderon. All rights reserved.
+//
+
+import Foundation
+
+let SHADOW_COLOR: CGFloat = 157.0 / 255.0
+
---
+++ DaycareManager/LoginViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LoginViewController.swift
+//  DaycareManager
+//
+//  Created by carlos calderon on 5/14/16.
+//  Copyright © 2016 carlos calderon. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
