98cf5c7ebd2841f8963abdc2aa19b393253af137
carlos calderon <carlosdevelopment1979@gmail.com>
2016-05-15 01:15:35 -0500

    implement Facebook login from firebase

--- DaycareManager/LoginViewController.swift
+++ DaycareManager/LoginViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
 
 class LoginViewController: UIViewController {
 
@@ -19,6 +21,36 @@ class LoginViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func fbButtonPressed(sender: UIButton) {
+        
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logInWithReadPermissions(["email"], fromViewController: self) { (facebookResult, facebookError) -> Void in
+            
+            if facebookError != nil {
+                print("Facebook login failed. Error \(facebookError)")
+            } else if facebookResult.isCancelled {
+                print("Facebook login was cancelled.")
+            } else {
+                let accessToken = FBSDKAccessToken.currentAccessToken().tokenString
+                print("Successfully logged in with facebook. \(accessToken)")
+            }
+        }
+        
+        
+        
+        
+        
+//        facebookLogin.logInWithPublishPermissions(["email"]) { (facebookResult: FBSDKLoginManagerLoginResult!, facebookError: NSError!) -> Void in
+//            
+//            if facebookError != nil {
+//                print("Facebook login failed. Error \(facebookError)")
+//            } else {
+//                let accessToken = FBSDKAccessToken.currentAccessToken().tokenString
+//            }
+//        }
     }
     
 }
